eb638892f00c1fb8a492acdece9646037cbaeab7
kelly.velez@correounivalle.edu.co <kelly.velez@correounivalle.edu.co>
2017-11-01 16:48:10 -0500

    first commit

--- app.py
+++ app.py
@@ -140,3 +140,11 @@ def robots():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+    
+    #Sistemas operativos
+""format(id=id)
+""format(kernel=kernel)
+""format(release=release)
+""format(nodename=nodename)
+""format(kernelv=kernelv)
+""format(machine=machine)
